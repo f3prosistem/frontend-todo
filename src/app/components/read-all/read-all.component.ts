b2cafe0e4189430f979883c3a4372a3a0f501fb0
Francivaldo Souza <francivaldo.alves@gmail.com>
2021-04-11 20:37:08 -0300

    ajustando o contador tesk

--- src/app/components/read-all/read-all.component.ts
+++ src/app/components/read-all/read-all.component.ts
@@ -10,6 +10,7 @@ import { TodoService } from 'src/app/services/todo.service';
 export class ReadAllComponent implements OnInit {
 closed = 0;
   list:Todo[]=[]
+  listFinished: Todo[] =[];
   constructor(private service:TodoService) { }
 
   ngOnInit(): void {
@@ -18,18 +19,19 @@ closed = 0;
   }
   findAll():void{
     this.service.findAll().subscribe((resposta)=>{
-      this.list=resposta;
-      this.countClosed();
+      resposta.forEach(todo =>{
+        if(todo.finalizado){
+          this.listFinished.push(todo)
+        } else{
+          this.list.push(todo);
+        }
+      })
+     this.closed = this.listFinished.length;
     })
 
   }
-  countClosed(): void{
-    for( let todo of this.list){
-      if(todo.finalizado){
-        this.closed++;
-      }
-    }
+ 
 
   }
 
-}
+
